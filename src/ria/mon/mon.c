3d76658d3a0674e6ca9c5a3b874e81f7e7c9cf1d
jasonfrowe <jasonfrowe@gmail.com>
2026-02-01 12:08:06 -0500

    undoing dumb edits #3

--- src/ria/mon/mon.c
+++ src/ria/mon/mon.c
@@ -4,10 +4,10 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include "mon/mon.h"
 #include "main.h"
 #include "mon/fil.h"
 #include "mon/hlp.h"
+#include "mon/mon.h"
 #include "mon/ram.h"
 #include "mon/rom.h"
 #include "mon/set.h"
@@ -18,12 +18,12 @@
 #include "sys/rln.h"
 #include "sys/sys.h"
 #include "sys/vga.h"
-#include <ctype.h>
 #include <fatfs/ff.h>
 #include <littlefs/lfs.h>
 #include <pico/stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #if defined(DEBUG_RIA_MON) || defined(DEBUG_RIA_MON_MON)
 #include <stdio.h>
@@ -36,8 +36,8 @@ static inline void DBG(const char *fmt, ...) { (void)fmt; }
 #define MON_RESPONSE_FN_COUNT 16
 static mon_response_fn mon_response_fn_list[MON_RESPONSE_FN_COUNT];
 static const char *mon_response_str[MON_RESPONSE_FN_COUNT];
-static int mon_response_state[MON_RESPONSE_FN_COUNT] = {
-    [0 ... MON_RESPONSE_FN_COUNT - 1] = -1};
+static int mon_response_state[MON_RESPONSE_FN_COUNT] =
+    {[0 ... MON_RESPONSE_FN_COUNT - 1] = -1};
 static int mon_response_line;
 static int mon_response_pos = -1;
 static bool mon_needs_newline = true;
@@ -56,7 +56,8 @@ static enum {
 } mon_more_state;
 
 typedef void (*mon_function)(const char *, size_t);
-__in_flash("mon_commands") static struct {
+__in_flash("mon_commands") static struct
+{
   const char *const cmd;
   mon_function func;
 } const MON_COMMANDS[] = {
@@ -80,34 +81,39 @@ __in_flash("mon_commands") static struct {
     {STR_UNLINK, fil_mon_unlink},
     {STR_BINARY, ram_mon_binary},
 };
-static const size_t MON_COMMANDS_COUNT =
-    sizeof MON_COMMANDS / sizeof *MON_COMMANDS;
+static const size_t MON_COMMANDS_COUNT = sizeof MON_COMMANDS / sizeof *MON_COMMANDS;
 
 // Returns NULL if not found. Advances buf to start of args.
-static mon_function mon_command_lookup(const char **buf, size_t buflen) {
+static mon_function mon_command_lookup(const char **buf, size_t buflen)
+{
   size_t i;
-  for (i = 0; i < buflen; i++) {
+    for (i = 0; i < buflen; i++)
+    {
     if ((*buf)[i] != ' ')
       break;
   }
   const char *cmd = (*buf) + i;
   bool is_maybe_addr = false;
   bool is_not_addr = false;
-  for (; i < buflen; i++) {
+    for (; i < buflen; i++)
+    {
     uint8_t ch = (*buf)[i];
     if (isxdigit(ch) || ch == '-')
       is_maybe_addr = true;
     else if (ch == ' ')
       break;
-    else if (ch == ':') {
+        else if (ch == ':')
+        {
       is_maybe_addr = true;
       i++;
       break;
-    } else
+        } 
+        else
       is_not_addr = true;
   }
   size_t cmd_len = (*buf) + i - cmd;
-  for (; i < buflen; i++) {
+  for (; i < buflen; i++)
+  {
     if ((*buf)[i] != ' ')
       break;
   }
@@ -115,17 +121,20 @@ static mon_function mon_command_lookup(const char **buf, size_t buflen) {
   if (cmd_len == 2 && !strncasecmp(cmd, STR_CD, cmd_len))
     is_not_addr = true;
   // 0:-7: and USB0:-USB7:
-  if (fil_drive_exists(cmd, cmd_len)) {
+    if (fil_drive_exists(cmd, cmd_len))
+    {
     *buf = cmd;
     return fil_mon_chdrive;
   }
   // address command
-  if (is_maybe_addr && !is_not_addr) {
+    if (is_maybe_addr && !is_not_addr)
+    {
     *buf = cmd;
     return ram_mon_address;
   }
   *buf += i;
-  for (i = 0; i < MON_COMMANDS_COUNT; i++) {
+    for (i = 0; i < MON_COMMANDS_COUNT; i++)
+    {
     if (cmd_len == strlen(MON_COMMANDS[i].cmd))
       if (!strncasecmp(cmd, MON_COMMANDS[i].cmd, cmd_len))
         return MON_COMMANDS[i].func;
@@ -137,7 +146,8 @@ bool mon_command_exists(const char *buf, size_t buflen) {
   return !!mon_command_lookup(&buf, buflen);
 }
 
-static void mon_enter(bool timeout, const char *buf, size_t length) {
+static void mon_enter(bool timeout, const char *buf, size_t length)
+{
   (void)timeout;
   assert(!timeout);
   mon_needs_prompt = true;
@@ -156,12 +166,14 @@ static void mon_enter(bool timeout, const char *buf, size_t length) {
 
 void mon_run_command(const char *cmd) { mon_enter(false, cmd, strlen(cmd)); }
 
-static int mon_str_response(char *buf, size_t buf_size, int state) {
+static int mon_str_response(char *buf, size_t buf_size, int state)
+{
   if (state < 0)
     return state;
   size_t i = 0;
   const char *str = mon_response_str[0];
-  for (; i + 1 < buf_size; i++) {
+    for (; i + 1 < buf_size; i++)
+    {
     char c = str[state];
     buf[i] = c;
     if (!c)
@@ -173,8 +185,10 @@ static int mon_str_response(char *buf, size_t buf_size, int state) {
   return state;
 }
 
-static const char *mon_lfs_lookup(int result) {
-  switch (-result) {
+static const char *mon_lfs_lookup(int result)
+{
+    switch (-result)
+    {
   case LFS_ERR_IO: // -5
     return STR_ERR_LFS_IO;
   case LFS_ERR_CORRUPT: // -84
@@ -208,7 +222,8 @@ static const char *mon_lfs_lookup(int result) {
   }
 }
 
-static int mon_lfs_response(char *buf, size_t buf_size, int state) {
+static int mon_lfs_response(char *buf, size_t buf_size, int state)
+{
   if (state < 0)
     return state;
   const char *err_str = mon_lfs_lookup(state);
@@ -219,8 +234,10 @@ static int mon_lfs_response(char *buf, size_t buf_size, int state) {
   return -1;
 }
 
-static const char *mon_fatfs_lookup(int fresult) {
-  switch (fresult) {
+static const char *mon_fatfs_lookup(int fresult)
+{
+    switch (fresult)
+    {
   case FR_DISK_ERR: // 1
     return STR_ERR_FATFS_DISK_ERR;
   case FR_INT_ERR: // 2
@@ -264,7 +281,8 @@ static const char *mon_fatfs_lookup(int fresult) {
   }
 }
 
-static int mon_fatfs_response(char *buf, size_t buf_size, int state) {
+static int mon_fatfs_response(char *buf, size_t buf_size, int state)
+{
   if (state < 0)
     return state;
   const char *err_str = mon_fatfs_lookup(state);
@@ -275,19 +293,22 @@ static int mon_fatfs_response(char *buf, size_t buf_size, int state) {
   return -1;
 }
 
-static void mon_append_response(mon_response_fn fn, const char *str,
-                                int state) {
+static void mon_append_response(mon_response_fn fn, const char *str, int state)
+{
   assert(state >= 0);
   int i = 0;
-  for (; i < MON_RESPONSE_FN_COUNT; i++) {
-    if (!mon_response_fn_list[i]) {
+    for (; i < MON_RESPONSE_FN_COUNT; i++)
+    {
+        if (!mon_response_fn_list[i])
+        {
       mon_response_fn_list[i] = fn;
       mon_response_str[i] = str;
       mon_response_state[i] = state;
       return;
     }
   }
-  if (i == MON_RESPONSE_FN_COUNT) {
+    if (i == MON_RESPONSE_FN_COUNT)
+    {
     i--;
     mon_response_fn_list[i] = mon_str_response;
     mon_response_str[i] = STR_ERR_MONITOR_RESPONSE_OVERFLOW;
@@ -295,9 +316,11 @@ static void mon_append_response(mon_response_fn fn, const char *str,
   }
 }
 
-static void mon_next_response(void) {
+static void mon_next_response(void)
+{
   int i = 0;
-  for (; i < MON_RESPONSE_FN_COUNT - 1; i++) {
+    for (; i < MON_RESPONSE_FN_COUNT - 1; i++)
+    {
     mon_response_fn_list[i] = mon_response_fn_list[i + 1];
     mon_response_str[i] = mon_response_str[i + 1];
     mon_response_state[i] = mon_response_state[i + 1];
@@ -307,11 +330,14 @@ static void mon_next_response(void) {
   mon_response_state[i] = -1;
 }
 
-static void mon_break_response(void) {
+static void mon_break_response(void)
+{
   mon_needs_break = false;
   mon_response_pos = -1;
-  for (int i = 0; i < MON_RESPONSE_FN_COUNT; i++) {
-    if (mon_response_state[i] >= 0) {
+    for (int i = 0; i < MON_RESPONSE_FN_COUNT; i++)
+    {
+        if (mon_response_state[i] >= 0)
+        {
       mon_response_fn_list[i](response_buf, RESPONSE_BUF_SIZE, -1);
       mon_response_fn_list[i] = NULL;
       mon_response_str[i] = NULL;
@@ -320,36 +346,44 @@ static void mon_break_response(void) {
   }
 }
 
-void mon_add_response_fn(mon_response_fn fn) {
+void mon_add_response_fn(mon_response_fn fn)
+{
   mon_append_response(fn, NULL, 0);
 }
 
-void mon_add_response_fn_state(mon_response_fn fn, int state) {
+void mon_add_response_fn_state(mon_response_fn fn, int state)
+{
   mon_append_response(fn, NULL, state);
 }
 
-void mon_add_response_str(const char *str) {
+void mon_add_response_str(const char *str)
+{
   mon_append_response(mon_str_response, str, 0);
 }
 
-void mon_add_response_lfs(int result) {
+void mon_add_response_lfs(int result)
+{
   if (result < 0)
     mon_append_response(mon_lfs_response, NULL, -result);
 }
 
-void mon_add_response_fatfs(int fresult) {
+void mon_add_response_fatfs(int fresult)
+{
   if (fresult > 0)
     mon_append_response(mon_fatfs_response, NULL, fresult);
 }
 
-static void mon_more(void) {
-  if (mon_needs_break) {
+static void mon_more(void)
+{
+    if (mon_needs_break)
+    {
     mon_needs_newline = false;
     if (mon_more_state == MON_MORE_START)
       return;
     mon_more_state = MON_MORE_END;
   }
-  switch (mon_more_state) {
+    switch (mon_more_state)
+    {
   case MON_MORE_START:
     printf(STR_MON_MORE_SHOW);
     mon_more_state = MON_MORE_FLUSH;
@@ -368,7 +402,8 @@ static void mon_more(void) {
     if (ch == '\30')
       mon_more_state = MON_MORE_C0;
     else if (ch != PICO_ERROR_TIMEOUT)
-      switch (mon_more_state) {
+            switch (mon_more_state)
+            {
       default: // MON_MORE_C0
         if (ch == '\33')
           mon_more_state = MON_MORE_ESC;
@@ -396,9 +431,11 @@ static void mon_more(void) {
   }
 }
 
-static int mon_guess_console_rows(void) {
+static int mon_guess_console_rows(void)
+{
   int rows = 24; // VT100 safe
-  if (vga_connected()) {
+    if (vga_connected())
+    {
     if (vga_get_display_type() == 2)
       rows = 32;
     else
@@ -407,7 +444,8 @@ static int mon_guess_console_rows(void) {
   return rows;
 }
 
-void mon_task(void) {
+void mon_task(void)
+{
   // The monitor must never print while 6502 is running.
   if (main_active())
     return;
@@ -416,11 +454,14 @@ void mon_task(void) {
   if (mon_needs_break)
     return mon_break_response();
   // Flush the current response buffer
-  if (mon_response_pos >= 0) {
+  if (mon_response_pos >= 0)
+  {
     int rows_max = mon_guess_console_rows() - 1;
     char c;
-    while ((c = response_buf[mon_response_pos]) && com_putchar_ready()) {
-      if (mon_response_line >= rows_max) {
+        while ((c = response_buf[mon_response_pos]) && com_putchar_ready())
+        {
+            if (mon_response_line >= rows_max)
+            {
         mon_more_state = MON_MORE_START;
         break;
       }
@@ -434,7 +475,8 @@ void mon_task(void) {
     return;
   }
   // Request the next response buffer
-  if (mon_response_pos == -1 && mon_response_state[0] >= 0) {
+    if (mon_response_pos == -1 && mon_response_state[0] >= 0)
+    {
     mon_response_pos = 0;
     response_buf[0] = 0;
     mon_response_state[0] = (mon_response_fn_list[0])(
@@ -444,10 +486,13 @@ void mon_task(void) {
     return;
   }
   // These can run the 6502 multiple times
-  if (ram_active() || rom_active() || fil_active())
+    if (ram_active() ||
+        rom_active() ||
+        fil_active())
     return;
   // The monitor has control
-  if (mon_needs_prompt) {
+    if (mon_needs_prompt)
+    {
     if (mon_needs_newline)
       mon_add_response_str(STR_MON_PROMPT_NEWLINE);
     else
@@ -458,7 +503,8 @@ void mon_task(void) {
     mon_response_line = 0;
     return;
   }
-  if (mon_needs_read_line) {
+    if (mon_needs_read_line)
+    {
     mon_needs_read_line = false;
     mon_response_line = 0;
     rln_read_line(0, mon_enter, 256, 0);
@@ -466,7 +512,8 @@ void mon_task(void) {
   }
 }
 
-void mon_break(void) {
+void mon_break(void)
+{
   mon_needs_prompt = true;
   mon_needs_newline = true;
   mon_needs_break = true;
