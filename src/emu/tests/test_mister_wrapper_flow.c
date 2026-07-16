ac44657345eb3a56b9498e35f5c8b5ee4c8bb55e
Jason Rowe <jasonfrowe@gmail.com>
2026-07-16 18:44:25 -0400

    Harden MiSTer wrapper payload flow tests

--- src/emu/tests/test_mister_wrapper_flow.c
+++ src/emu/tests/test_mister_wrapper_flow.c
@@ -63,12 +63,12 @@ UTEST(mister_wrapper, launch_stop_reset_flow)
 
     fd = mkstemp(path);
     ASSERT_GT(fd, 0);
-    ASSERT_EQ(ftruncate(fd, RP6502_ARM_IF_REG_SIZE), 0);
+    ASSERT_EQ(ftruncate(fd, RP6502_ARM_IF_WINDOW_SIZE), 0);
 
-    map = mmap(NULL, RP6502_ARM_IF_REG_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    map = mmap(NULL, RP6502_ARM_IF_WINDOW_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     ASSERT_NE(map, MAP_FAILED);
     regs = (volatile uint32_t *)map;
-    memset((void *)regs, 0, RP6502_ARM_IF_REG_SIZE);
+    memset((void *)regs, 0, RP6502_ARM_IF_WINDOW_SIZE);
 
     child = fork();
     ASSERT_GE(child, 0);
@@ -132,7 +132,63 @@ UTEST(mister_wrapper, launch_stop_reset_flow)
     ASSERT_EQ(kill(child, SIGTERM), 0);
     ASSERT_EQ(waitpid(child, NULL, 0), child);
 
-    ASSERT_EQ(munmap((void *)regs, RP6502_ARM_IF_REG_SIZE), 0);
+    ASSERT_EQ(munmap((void *)regs, RP6502_ARM_IF_WINDOW_SIZE), 0);
+    ASSERT_EQ(close(fd), 0);
+    ASSERT_EQ(unlink(path), 0);
+}
+
+UTEST(mister_wrapper, invalid_payload_path_sets_error)
+{
+    char path[] = "/tmp/rp6502-wrapper-flow-XXXXXX";
+    int fd;
+    void *map;
+    volatile uint32_t *regs;
+    pid_t child;
+
+    fd = mkstemp(path);
+    ASSERT_GT(fd, 0);
+    ASSERT_EQ(ftruncate(fd, RP6502_ARM_IF_WINDOW_SIZE), 0);
+
+    map = mmap(NULL, RP6502_ARM_IF_WINDOW_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    ASSERT_NE(map, MAP_FAILED);
+    regs = (volatile uint32_t *)map;
+    memset((void *)regs, 0, RP6502_ARM_IF_WINDOW_SIZE);
+
+    child = fork();
+    ASSERT_GE(child, 0);
+
+    if (child == 0)
+    {
+        execl(MISTER_WRAPPER_BIN,
+              MISTER_WRAPPER_BIN,
+              "--transport",
+              "file",
+              "--reg-file",
+              path,
+              "--poll-ms",
+              "1",
+              (char *)NULL);
+        _exit(127);
+    }
+
+    ASSERT_TRUE(wait_until_eq(regs, RP6502_ARM_IF_OFF_MAGIC, RP6502_ARM_IF_MAGIC, 500));
+
+    strcpy((char *)regs + RP6502_ARM_PAYLOAD_V1_OFF_RUNTIME_PATH, "bin/sleep");
+    rp6502_arm_if_write(regs, RP6502_ARM_IF_OFF_CTRL, RP6502_ARM_CTRL_REQ_LAUNCH);
+    rp6502_arm_if_write(regs, RP6502_ARM_IF_OFF_CMD_SEQ, 1);
+
+    ASSERT_TRUE(wait_until_eq(regs, RP6502_ARM_IF_OFF_ACK_SEQ, 1, 500));
+    ASSERT_TRUE(wait_until_eq(regs, RP6502_ARM_IF_OFF_LAST_ERROR, RP6502_ARM_ERR_BAD_PAYLOAD_PATH, 500));
+    ASSERT_TRUE(wait_until_mask(regs,
+                                RP6502_ARM_IF_OFF_STATUS,
+                                RP6502_ARM_STATUS_RUNNING | RP6502_ARM_STATUS_ERROR,
+                                RP6502_ARM_STATUS_ERROR,
+                                500));
+
+    ASSERT_EQ(kill(child, SIGTERM), 0);
+    ASSERT_EQ(waitpid(child, NULL, 0), child);
+
+    ASSERT_EQ(munmap((void *)regs, RP6502_ARM_IF_WINDOW_SIZE), 0);
     ASSERT_EQ(close(fd), 0);
     ASSERT_EQ(unlink(path), 0);
 }
