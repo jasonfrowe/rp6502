ada5edf8b82a3e6d3bbd6f98b9b83ca2b35b4758
Jason Rowe <jasonfrowe@gmail.com>
2026-07-16 18:34:30 -0400

    Add MiSTer wrapper flow integration test

---
+++ src/emu/tests/test_mister_wrapper_flow.c
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2026 Rumbledethumps
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "utest.h"
+
+#include "arm_if_v1.h"
+
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+static bool wait_until_eq(volatile uint32_t *regs, uint32_t off, uint32_t expected, int timeout_ms)
+{
+    int i;
+    for (i = 0; i < timeout_ms; i++)
+    {
+        if (rp6502_arm_if_read(regs, off) == expected)
+        {
+            return true;
+        }
+        usleep(1000);
+    }
+    return false;
+}
+
+static bool wait_until_mask(volatile uint32_t *regs, uint32_t off, uint32_t mask, uint32_t expected, int timeout_ms)
+{
+    int i;
+    for (i = 0; i < timeout_ms; i++)
+    {
+        if ((rp6502_arm_if_read(regs, off) & mask) == expected)
+        {
+            return true;
+        }
+        usleep(1000);
+    }
+    return false;
+}
+
+UTEST(mister_wrapper, launch_stop_reset_flow)
+{
+    char path[] = "/tmp/rp6502-wrapper-flow-XXXXXX";
+    int fd;
+    void *map;
+    volatile uint32_t *regs;
+    pid_t child;
+    uint32_t hb0;
+    uint32_t hb1;
+
+    fd = mkstemp(path);
+    ASSERT_GT(fd, 0);
+    ASSERT_EQ(ftruncate(fd, RP6502_ARM_IF_REG_SIZE), 0);
+
+    map = mmap(NULL, RP6502_ARM_IF_REG_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    ASSERT_NE(map, MAP_FAILED);
+    regs = (volatile uint32_t *)map;
+    memset((void *)regs, 0, RP6502_ARM_IF_REG_SIZE);
+
+    child = fork();
+    ASSERT_GE(child, 0);
+
+    if (child == 0)
+    {
+        execl(MISTER_WRAPPER_BIN,
+              MISTER_WRAPPER_BIN,
+              "--transport",
+              "file",
+              "--reg-file",
+              path,
+              "--runtime",
+              "/bin/sleep",
+              "--runtime-arg",
+              "5",
+              "--poll-ms",
+              "1",
+              (char *)NULL);
+        _exit(127);
+    }
+
+    ASSERT_TRUE(wait_until_eq(regs, RP6502_ARM_IF_OFF_MAGIC, RP6502_ARM_IF_MAGIC, 500));
+    ASSERT_TRUE(wait_until_eq(regs, RP6502_ARM_IF_OFF_VERSION, RP6502_ARM_IF_VERSION, 500));
+    ASSERT_TRUE(wait_until_mask(regs,
+                                RP6502_ARM_IF_OFF_STATUS,
+                                RP6502_ARM_STATUS_PRESENT | RP6502_ARM_STATUS_READY,
+                                RP6502_ARM_STATUS_PRESENT | RP6502_ARM_STATUS_READY,
+                                500));
+
+    hb0 = rp6502_arm_if_read(regs, RP6502_ARM_IF_OFF_HEARTBEAT);
+    usleep(8000);
+    hb1 = rp6502_arm_if_read(regs, RP6502_ARM_IF_OFF_HEARTBEAT);
+    ASSERT_GT(hb1, hb0);
+
+    rp6502_arm_if_write(regs, RP6502_ARM_IF_OFF_CTRL, RP6502_ARM_CTRL_REQ_LAUNCH);
+    rp6502_arm_if_write(regs, RP6502_ARM_IF_OFF_CMD_SEQ, 1);
+
+    ASSERT_TRUE(wait_until_eq(regs, RP6502_ARM_IF_OFF_ACK_SEQ, 1, 500));
+    ASSERT_TRUE(wait_until_mask(regs,
+                                RP6502_ARM_IF_OFF_STATUS,
+                                RP6502_ARM_STATUS_RUNNING | RP6502_ARM_STATUS_ERROR,
+                                RP6502_ARM_STATUS_RUNNING,
+                                500));
+
+    rp6502_arm_if_write(regs, RP6502_ARM_IF_OFF_CTRL, RP6502_ARM_CTRL_REQ_STOP);
+    rp6502_arm_if_write(regs, RP6502_ARM_IF_OFF_CMD_SEQ, 2);
+
+    ASSERT_TRUE(wait_until_eq(regs, RP6502_ARM_IF_OFF_ACK_SEQ, 2, 500));
+    ASSERT_TRUE(wait_until_mask(regs,
+                                RP6502_ARM_IF_OFF_STATUS,
+                                RP6502_ARM_STATUS_RUNNING,
+                                0,
+                                500));
+
+    rp6502_arm_if_write(regs, RP6502_ARM_IF_OFF_CTRL, RP6502_ARM_CTRL_REQ_RESET_RUNTIME);
+    rp6502_arm_if_write(regs, RP6502_ARM_IF_OFF_CMD_SEQ, 3);
+
+    ASSERT_TRUE(wait_until_eq(regs, RP6502_ARM_IF_OFF_ACK_SEQ, 3, 500));
+    ASSERT_TRUE(wait_until_eq(regs, RP6502_ARM_IF_OFF_LAST_ERROR, RP6502_ARM_ERR_NONE, 500));
+
+    ASSERT_EQ(kill(child, SIGTERM), 0);
+    ASSERT_EQ(waitpid(child, NULL, 0), child);
+
+    ASSERT_EQ(munmap((void *)regs, RP6502_ARM_IF_REG_SIZE), 0);
+    ASSERT_EQ(close(fd), 0);
+    ASSERT_EQ(unlink(path), 0);
+}
+
+UTEST_MAIN()
